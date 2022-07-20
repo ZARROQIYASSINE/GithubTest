0c3a5c8461f58a22159c5af61cab5358be220a0a
YASSINE ZARROQI <96484545+ZARROQIYASSINE@users.noreply.github.com>
2022-07-20 17:19:24 +0200

    Create data.php

---
+++ data.php
@@ -0,0 +1,66 @@
+<?php
+
+$data = [
+	[
+		"name" => "Britanni Howell",
+		"email" => "non.lorem@hotmail.org",
+		"date" => "Apr 21, 2022",
+		"postalZip" => "24-42"
+	],
+	[
+		"name" => "Sydney Gaines",
+		"email" => "aliquet.diam@outlook.org",
+		"date" => "Aug 14, 2021",
+		"postalZip" => "H2N 8C1"
+	],
+	[
+		"name" => "Rebecca Humphrey",
+		"email" => "neque@google.edu",
+		"date" => "Mar 31, 2023",
+		"postalZip" => "7358"
+	],
+	[
+		"name" => "Tatiana Farley",
+		"email" => "magnis.dis@yahoo.org",
+		"date" => "Apr 14, 2022",
+		"postalZip" => "65-74"
+	],
+	[
+		"name" => "Malachi Myers",
+		"email" => "mollis.lectus@outlook.org",
+		"date" => "Jul 27, 2022",
+		"postalZip" => "442877"
+	],
+	[
+		"name" => "Blythe Oliver",
+		"email" => "eu.euismod@outlook.ca",
+		"date" => "Sep 3, 2022",
+		"postalZip" => "0134"
+	],
+	[
+		"name" => "Summer Landry",
+		"email" => "sit@yahoo.com",
+		"date" => "Oct 19, 2022",
+		"postalZip" => "813212"
+	],
+	[
+		"name" => "Mariko Hancock",
+		"email" => "donec@outlook.com",
+		"date" => "Oct 26, 2022",
+		"postalZip" => "173374"
+	],
+	[
+		"name" => "Raphael Prince",
+		"email" => "lacus.aliquam@hotmail.ca",
+		"date" => "Jul 19, 2023",
+		"postalZip" => "4341-1298"
+	],
+	[
+		"name" => "Maggie Guzman",
+		"email" => "vulputate.dui@yahoo.couk",
+		"date" => "Oct 6, 2022",
+		"postalZip" => "652385"
+	]
+];
+
+?>
